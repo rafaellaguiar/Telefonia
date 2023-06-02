946559eb312a3ec7c76173f2575722ac150f962b
Rafael Aguiar <rafael.l.aguiar0219@gmail.com>
2023-06-02 00:02:54 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/HomeController/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.HomeController
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            List<string> telefones = GetListaTelefones();
+
+            ViewData["ListaTelefones"] = telefones;
+
+            return View();
+        }
+        private List<string> GetListaTelefones()
+        {
+            var lista = new List<string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add("TELEFONO" + i.ToString());
+            }
+
+            return lista;
+        }
+    }
+}
